e3c6fbf6a080f4a251a4496f8a531a660f1a78bc
Isabel Queiroz <belqrdev@outlook.com>
2022-06-09 17:29:50 -0300

    Ok!

---
+++ index.js
@@ -0,0 +1,19 @@
+//Estrutura de Dados
+
+let name = 'Pandora';
+let number = 73;
+let ano = 2022;
+let quartaFeira = true;
+
+console.log(name);
+console.log(ano - number);
+console.log("Estou mexendo no console.log!")
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
+
+
+
+
+
+
+
